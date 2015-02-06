f5b576351217698bc97c752ae6a142fb29fe3738
Louis Beaudoin <louis@embedded-creations.com>
2015-02-06 15:23:57 -0500

    turn off LED before starting app

--- JumpToAppWithOffset.ino
+++ JumpToAppWithOffset.ino
@@ -79,6 +79,8 @@ void loop() {
   delay(1000);               // wait for a second
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  pinMode(led, INPUT);
 
   __disable_irq();
   resetPeripherals();
